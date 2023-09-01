f5087e9094c2fca22fca85894cc1833072e02d7b
sara.exe <97038936+saraferreira10@users.noreply.github.com>
2023-09-01 09:54:59 -0300

    Botão de cadastro funcionando

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -7,36 +7,31 @@
  *
  * @author Adm
  */
-
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
-
 public class ProdutosDAO {
-    
+
     Connection conn;
     PreparedStatement prep;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
-    public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+
+    public void cadastrarProduto(ProdutosDTO produto) throws SQLException {
+        conn = new conectaDAO().connectDB();
+        prep = conn.prepareStatement("INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)");
+        prep.setString(1, produto.getNome());
+        prep.setInt(2, produto.getValor());
+        prep.setString(3, produto.getStatus());
+        prep.executeUpdate();
     }
-    
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
+
+    public ArrayList<ProdutosDTO> listarProdutos() {
         return listagem;
     }
-    
-    
-    
-        
-}
 
+}
